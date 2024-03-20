83ede7fa849c85c076b829dd4a5f29521003d3d7
robyschumi <146774581+robigitavela@users.noreply.github.com>
2024-03-21 00:48:00 +0100

    Create React.dom-01.jsx

---
+++ React.dom-01.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Izgleada go ukapirav</h2>
+        <h3>Aj ke vidime</h3>
+        <img
+          src="https://mcn-images.bauersecure.com/wp-images/4775/2021-bmw-c400gt.jpg"
+          alt="picture"
+          className="BMW-jpg"
+        />
+        <h1>MOZDA I KE ME BIDE???</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
